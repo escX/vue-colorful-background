fb25166b3d01006114fa8f268975b4c2614d6f8d
escX <1768810189@qq.coom>
2019-03-29 18:22:57 +0800

    realizes

--- src/app.vue
+++ src/app.vue
@@ -1,11 +1,30 @@
 <template>
-  <div id="app" v-colorful>
-    test
+  <div id="app">
+    <div v-colorful.random="config">
+      {{test}}
+    </div>
+
+    <demo v-colorful></demo>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      test: 'colorful',
+      config: {
+        duration: '2s',
+        timing: 'ease-in',
+        colors: ['#000', '#fff'],
+        delay: 2000
+      }
+    }
+  },
+  components: {
+    'demo': {
+      template: '<div id="demo">demo</div>'
+    }
+  }
 }
 </script>
