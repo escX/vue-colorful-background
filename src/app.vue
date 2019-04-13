d6a5b4ed723930d8a5ad72461701044c3d07dcba
escX <1768810189@qq.com>
2019-04-13 22:00:56 +0800

    improve example

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,24 @@
 <template>
-  <div id="app" v-colorful.random>
-    <component-demo v-colorful="colorfulConfig"></component-demo>
+  <div id="app">
+    <div class="demo1">
+      <div class="container" v-colorful.random>
+        <pre>v-colorful.random</pre>
+      </div>
+    </div>
+    <div class="demo2">
+      <div class="container" v-colorful="colorfulConfig">
+<pre>v-colorful="colorfulConfig"
+
+colorfulConfig: {
+  duration: '1s',
+  timing: 'linear',
+  colors: ['#000', '#fff'],
+  delay: 3000
+}</pre>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -9,37 +26,40 @@ export default {
   data() {
     return {
       colorfulConfig: {
-        duration: '2s',
-        timing: 'ease-in',
-        colors: ['#000', '#333', '#666', '#999', '#ccc', '#fff'],
-        delay: 2000,
-      },
+        duration: '1s',
+        timing: 'linear',
+        colors: ['#000', '#fff'],
+        delay: 3000
+      }
     };
-  },
-  components: {
-    'component-demo': {
-      render: h => h('div', {
-        style: {
-          width: '100px',
-          height: '100px',
-          position: 'absolute',
-          left: '50%',
-          top: '50%',
-          transform: 'translate(-50%, -50%)'
-        },
-      }),
-    }
   }
 };
 </script>
 
 <style>
-html, body {
-  margin: 0;
-  height: 100%;
-}
-#app {
-  width: 100%;
-  height: 100%;
-}
+  .container {
+    width: 100%;
+    padding: 50px 0;
+    position: relative;
+  }
+  .demo1 {
+    text-align: center;
+  }
+  .demo2 {
+    margin-top: 50px;
+  }
+  .demo1 .container {
+    height: 120px;
+  }
+  .demo2 .container {
+    height: 300px;
+  }
+  pre {
+    background-color: #fff;
+    box-shadow: 0 0 10px 0 #000;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 </style>
